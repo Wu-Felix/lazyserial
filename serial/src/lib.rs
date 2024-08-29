ab41f9497aabc8056146be1c56da2e27ae641634
felix <Wu-Felix@outlook.com>
2024-08-29 11:34:26 +0800

    feat(serial):查询本机串口列表

---
+++ serial/src/lib.rs
@@ -0,0 +1,49 @@
+pub mod serial {
+    #[derive(Debug)]
+    pub struct Serial {
+        pub baud: Option<u32>,
+        pub port: Option<String>,
+    }
+    pub trait SerialInfo {
+        fn print(&self);
+        fn serial_port_list(&self);
+    }
+    impl<U> SerialInfo for U
+    where
+        U: std::fmt::Debug,
+    {
+        fn print(&self) {
+            println!("{:#?}", self);
+        }
+        fn serial_port_list(&self) {
+            match serialport::available_ports() {
+                Ok(serial_port_info) => {
+                    let serial_name: Vec<_> = serial_port_info
+                        .iter()
+                        .map(|f| &f.port_name)
+                        .zip(serial_port_info.iter().map(|f| {
+                            if let serialport::SerialPortType::UsbPort(serialport::UsbPortInfo {
+                                product,
+                                ..
+                            }) = &f.port_type
+                            {
+                                return product.to_owned();
+                            }
+                            None
+                        }))
+                        .collect();
+                    eprintln!("{:#?}", serial_name);
+                }
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+    }
+    impl Serial {
+        pub fn build() -> Self {
+            Self {
+                baud: None,
+                port: None,
+            }
+        }
+    }
+}
